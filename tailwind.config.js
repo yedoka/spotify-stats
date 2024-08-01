3831a30563f1c9e3a77145c021074b7646f035e8
yedoka <132571700+yedoka@users.noreply.github.com>
2024-08-01 15:05:00 +0200

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        spotify: "#1DB954",
+        spotifyDark: "#157a39",
+      },
+      keyframes: {
+        fadeIn: {
+          "0%": { opacity: 0 },
+          "100%": { opacity: 1 },
+        },
+      },
+      animation: {
+        fadeIn: "fadeIn 1s ease-in-out",
+      },
+    },
+  },
+  plugins: [],
+};
